bcfd377e2fb8819580ab9c3bc5de1e9296ead2ea
sassiddhik86 <sassiddhik@yahoo.com>
2026-08-11 23:30:25 +0800

    Code cleanup

--- ChubbInsuranceClaim/src/Domain/Common/IAuditable.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace ChubbInsuranceClaim.src.Domain.Common
-{
-    public interface IAuditable
-    {
-        DateTime CreatedDate { get; set; }
-
-        DateTime? UpdatedDate { get; set; }
-    }
-}
--- ChubbInsuranceClaim/src/Domain/Common/ISoftDelete.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace ChubbInsuranceClaim.src.Domain.Common
-{
-    public interface ISoftDelete
-    {
-        bool IsDeleted { get; set; }
-
-        DateTime? DeletedOn { get; set; }
-    }
-}
